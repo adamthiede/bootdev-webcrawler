723daae27a54d613b75cbe2b67610fcf6a684481
Adam Thiede <me@adamthiede.com>
2024-05-03 06:40:28 -0500

    add more tests

--- crawl.test.js
+++ crawl.test.js
@@ -12,3 +12,18 @@ test('url1 should be url2', () => {
 test('url1 should not be url3', () => {
   expect(u1==u2).toBe(true);
 });
+
+test('strip paths', () => {
+  expect(normalizeURL("http://github.com")=="github.com").toBe(true);
+});
+
+test('no trailing slash', () => {
+  expect(normalizeURL("http://github.com/")=="github.com").toBe(true);
+});
+test('no trailing slash v2', () => {
+  expect(normalizeURL("http://github.com/")=="github.com/").toBe(false);
+});
+
+test('strip most parameters', () => {
+  expect(normalizeURL("rsync://user:password@github.com:2222/path?repo=test")=="github.com/path").toBe(true);
+});
